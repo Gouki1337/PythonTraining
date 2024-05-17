6611e8b2ea1f2dd2377e9226155aff8e8f6a48a5
NULLptr <stefan.grybowski@outlook.com>
2024-05-17 11:43:14 +0200

    Added day 1 - task 9

---
+++ day1/task9.py
@@ -0,0 +1,21 @@
+from ast import arg
+from ssl import Options
+import sys
+import getopt
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    input = None
+
+    try:
+        options, args = getopt.getopt(argv, "s:")
+    except Exception as e:
+        print(f"Error message! {e}")
+
+    for arg, value in options:
+        if arg in ["-s"]:
+            sentence = value
+            
+    numOfWords = len(sentence.split())
+    print("Input sentence:", sentence)
+    print("Counted words in senctence:", numOfWords)
