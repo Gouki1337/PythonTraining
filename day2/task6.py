3011406802a360543cf4312c4187f5452b2c531b
NULLptr <stefan.grybowski@outlook.com>
2024-06-03 14:48:03 +0200

    Added day 2 - task 6

---
+++ day2/task6.py
@@ -0,0 +1,22 @@
+from ctypes import sizeof
+import string
+
+def isPangram(input):
+    alphabet = list(string.ascii_lowercase)
+
+    for char in alphabet:
+        if char not in input.lower():
+            return False
+
+    return True
+
+def main():
+    txt1 = "this is a test string"
+    txt2 = "a quick brown fox jumps over the lazy dog"
+
+    print("Text1:", isPangram(txt1))
+    print("Text2:", isPangram(txt2))
+    
+
+if __name__ == '__main__': 
+    main()
