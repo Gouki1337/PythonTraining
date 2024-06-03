44a2efc26fc448ced7f7adcd9580c159493e7173
NULLptr <stefan.grybowski@outlook.com>
2024-06-03 11:44:38 +0200

    Added day 2 - task 4

---
+++ day2/task4.py
@@ -0,0 +1,7 @@
+def reverseString(input):
+    return input[::-1]
+
+if __name__ == '__main__':
+    inputString = "abcd"
+    print("Input string:", inputString)
+    print("Reverse string:", reverseString(inputString))
